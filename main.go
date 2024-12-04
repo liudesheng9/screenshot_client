7c5a5789c7b23f6c633a7c36265038021a30ef8e
“liudesheng9” <chenxt22@mails.tsinghua.edu.cn>
2024-12-04 14:48:30 -0800

    update client

--- main.go
+++ main.go
@@ -13,12 +13,24 @@ import (
 var Global_sig_run int = 1
 var Global_sig_lock sync.Mutex
 
+func title_print() {
+	fmt.Println(`____    ____  ____   _____  _____  _   _  ____   _   _   ___  _____ `)
+	fmt.Println(`/ ___|  / ___||  _ \ | ____|| ____|| \ | |/ ___| | | | | / _ \|_   _|`)
+	fmt.Println(`\___ \ | |    | |_) ||  _|  |  _|  |  \| |\___ \ | |_| || | | | | |  `)
+	fmt.Println(` ___) || |___ |  _ < | |___ | |___ | |\  | ___) ||  _  || |_| | | |  `)
+	fmt.Println(`|____/  \____||_| \_\|_____||_____||_| \_||____/ |_| |_| \___/  |_|  `)
+	fmt.Println("CLIENT")
+}
+
 func main() {
+	title_print()
+	fmt.Println("Waiting for server...")
 	for {
 		//connect to server
 		conn, err := net.Dial("tcp", "127.0.0.1:50021")
 		if err != nil {
-			fmt.Printf("dial failed, err:%v\n", err)
+			// fmt.Printf("dial failed, err:%v\n", err)
+			time.Sleep(5 * time.Second)
 			continue
 		}
 
@@ -72,6 +84,10 @@ func main() {
 					continue
 				}
 				fmt.Println("Get server message: ", string(buf[:n]))
+				if string(buf[:n]) == "server close" {
+					fmt.Printf("server closed\n")
+					return
+				}
 			}
 		}()
 		wg.Wait()
