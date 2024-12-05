51a74b2dd03197c14a2bc739fc610327f4bf6275
“liudesheng9” <chenxt22@mails.tsinghua.edu.cn>
2024-12-05 01:09:57 -0800

    update flow

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module screenshot_client
 
 go 1.23.3
+
+require github.com/petermattis/goid v0.0.0-20241025130422-66cb2e6d7274 // indirect
--- main.go
+++ main.go
@@ -12,9 +12,26 @@ import (
 
 var Global_sig_run int = 1
 var Global_sig_lock sync.Mutex
+var Global_sig_current_run int = 1
+var Global_sig_current_run_lock sync.Mutex
+var Global_writer_end int = 0
+
+type Task func(args ...interface{}) (interface{}, error)
+
+func retry_task(task Task, args ...interface{}) interface{} {
+	for {
+		result, err := task(args...)
+		if err == nil {
+			return result
+		} else {
+			fmt.Printf("Error: %v\n", err)
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
 
 func title_print() {
-	fmt.Println(`____    ____  ____   _____  _____  _   _  ____   _   _   ___  _____ `)
+	fmt.Println(` ____    ____  ____   _____  _____  _   _  ____   _   _   ___  _____ `)
 	fmt.Println(`/ ___|  / ___||  _ \ | ____|| ____|| \ | |/ ___| | | | | / _ \|_   _|`)
 	fmt.Println(`\___ \ | |    | |_) ||  _|  |  _|  |  \| |\___ \ | |_| || | | | | |  `)
 	fmt.Println(` ___) || |___ |  _ < | |___ | |___ | |\  | ___) ||  _  || |_| | | |  `)
@@ -24,47 +41,83 @@ func title_print() {
 
 func main() {
 	title_print()
-	fmt.Println("Waiting for server...")
+	input_channel := make(chan string)
+	go func() {
+		input := bufio.NewReader(os.Stdin)
+		for {
+			s, _ := input.ReadString('\n')
+			s = strings.TrimSpace(s)
+			if strings.ToUpper(s) == "Q" {
+				Global_sig_lock.Lock()
+				Global_sig_run = 0
+				Global_sig_lock.Unlock()
+				Global_writer_end = 1
+				Global_sig_current_run_lock.Lock()
+				Global_sig_current_run = 0
+				Global_sig_current_run_lock.Unlock()
+				return
+			}
+			if strings.ToUpper(s) == "NAN" {
+				continue
+			}
+			input_channel <- s
+		}
+	}()
 	for {
+		Global_sig_current_run_lock.Lock()
+		Global_sig_current_run = 1
+		Global_sig_current_run_lock.Unlock()
+		fmt.Println("Waiting for server...")
 		//connect to server
-		conn, err := net.Dial("tcp", "127.0.0.1:50021")
-		if err != nil {
-			// fmt.Printf("dial failed, err:%v\n", err)
-			time.Sleep(5 * time.Second)
-			continue
+	ini_link:
+		task_net_dial := func(args ...interface{}) (interface{}, error) {
+			conn, err := net.Dial("tcp", args[0].(string))
+			return conn, err
 		}
-
-		conn.Write([]byte("hello server"))
-		var buf [1024]byte
-		n, err := conn.Read(buf[:])
-		if err != nil {
-			fmt.Printf("read failed, err:%v\n", err)
-			continue
-		}
-		if string(buf[:n]) == "1" {
-			fmt.Println("connect success")
+		conn := retry_task(task_net_dial, "127.0.0.1:50021").(net.Conn)
+		fmt.Println("connect established")
+		for {
+			conn.Write([]byte("hello server"))
+			var buf [1024]byte
+			conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			n, err := conn.Read(buf[:])
+			if err != nil {
+				if opErr, ok := err.(net.Error); ok && opErr.Timeout() {
+					continue
+				}
+				fmt.Println("Error reading from connection:", err)
+				time.Sleep(5 * time.Second)
+				goto ini_link
+			}
+			if string(buf[:n]) == "1" {
+				fmt.Println("connect success")
+				break
+			}
 		}
 		wg := sync.WaitGroup{}
 		wg.Add(2)
 		// two go routine, one for send message, one for receive message
-		input := bufio.NewReader(os.Stdin)
 		go func() {
 			defer wg.Done()
 			for {
-				s, _ := input.ReadString('\n')
-				s = strings.TrimSpace(s)
-				if strings.ToUpper(s) == "Q" {
-					Global_sig_lock.Lock()
-					Global_sig_run = 0
-					Global_sig_lock.Unlock()
+				if Global_sig_current_run == 0 {
 					return
 				}
-				//send message to server
-				_, err := conn.Write([]byte(s))
-				if err != nil {
-					fmt.Printf("send failed,err:%v\n", err)
+				if Global_sig_run == 0 {
+					return
+				}
+				select {
+				case s := <-input_channel:
+					//send message to server
+					_, err := conn.Write([]byte(s))
+					if err != nil {
+						fmt.Printf("send failed,err:%v\n", err)
+						continue
+					}
+				case <-time.After(5 * time.Second):
 					continue
 				}
+
 			}
 		}()
 		go func() {
@@ -86,6 +139,9 @@ func main() {
 				fmt.Println("Get server message: ", string(buf[:n]))
 				if string(buf[:n]) == "server close" {
 					fmt.Printf("server closed\n")
+					Global_sig_current_run_lock.Lock()
+					Global_sig_current_run = 0
+					Global_sig_current_run_lock.Unlock()
 					return
 				}
 			}
@@ -93,7 +149,12 @@ func main() {
 		wg.Wait()
 		conn.Close()
 		if Global_sig_run == 0 {
-			break
+			for {
+				time.Sleep(1 * time.Second)
+				if Global_writer_end == 1 {
+					return
+				}
+			}
 		}
 	}
 }
